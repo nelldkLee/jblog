178823b9cb10a7fb009f12c9457efa532f5eec0d
nelldkLee <nelldklee@gmail.com>
2019-06-03 16:43:54 +0900

    불필요한 apache.catalina 패키지 import로 인한 참조 에러

--- jblog2/src/main/java/com/cafe24/jblog/controller/BlogController.java
+++ jblog2/src/main/java/com/cafe24/jblog/controller/BlogController.java
@@ -3,7 +3,6 @@ package com.cafe24.jblog.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.LifecycleListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +22,7 @@ import com.cafe24.jblog.vo.BlogVo;
 import com.cafe24.jblog.vo.CategoryVo;
 import com.cafe24.jblog.vo.JSONResult;
 import com.cafe24.jblog.vo.PostVo;
+
 
 @Controller
 @RequestMapping("/{blog_id:(?!assets|images).*}")
